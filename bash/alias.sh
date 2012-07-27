67e80bfd917c0fa865b7dc891622968035c74043
sunteya <sunteya@gmail.com>
2012-07-27 12:29:27 +0800

    alias guard to bundle exec guard

--- bash/alias.sh
+++ bash/alias.sh
@@ -1,4 +1,4 @@
-BUNDLED_COMMANDS="rackup rails rake rspec ruby spork thin unicorn annotate"
+BUNDLED_COMMANDS="rackup rails rake rspec guard ruby spork thin unicorn annotate"
 [ -f $HOME/.bash/bundler-exec.sh ] && source $HOME/.bash/bundler-exec.sh
 
 alias ls-al='ls -al'
