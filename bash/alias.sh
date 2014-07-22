67bf93f14aac2b90d55f9932b84fa66f946b74e4
sunteya <sunteya@gmail.com>
2014-07-22 09:49:28 +0800

    now the ‘cap’ is a bundle command

--- bash/alias.sh
+++ bash/alias.sh
@@ -1,4 +1,4 @@
-BUNDLED_COMMANDS="rackup rails rake rspec guard ruby spork thin unicorn annotate"
+BUNDLED_COMMANDS="rackup rails rake rspec guard ruby spork thin unicorn annotate cap"
 [ -f $HOME/.bash/bundler-exec.sh ] && source $HOME/.bash/bundler-exec.sh
 
 alias ls-al='ls -al'
