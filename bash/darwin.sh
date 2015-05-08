0fee8e92a1dd0a0a36f292069ddf51b681e8feb3
sunteya <sunteya@gmail.com>
2015-05-08 14:34:45 +0800

    update autojump

--- bash/darwin.sh
+++ bash/darwin.sh
@@ -4,7 +4,7 @@
 
 if [[ $(type -P brew) ]]; then
 	[[ -f `brew --prefix`/etc/bash_completion ]] && source `brew --prefix`/etc/bash_completion
-	[[ -f `brew --prefix`/etc/autojump.bash ]] && source `brew --prefix`/etc/autojump.bash
+	[[ -s $(brew --prefix)/etc/autojump.sh ]] && . $(brew --prefix)/etc/autojump.sh
 fi
 
 
