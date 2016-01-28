a819a7924644edff55e85b6b084f93fa38a7e6f6
sunteya <sunteya@gmail.com>
2016-01-28 09:06:29 +0800

    support bash completion 2

--- bash/darwin.sh
+++ bash/darwin.sh
@@ -4,6 +4,11 @@
 
 if [[ $(type -P brew) ]]; then
 	[[ -f `brew --prefix`/etc/bash_completion ]] && source `brew --prefix`/etc/bash_completion
+
+	if [ -f $(brew --prefix)/share/bash-completion/bash_completion  ]; then
+		. $(brew --prefix)/share/bash-completion/bash_completion
+	fi
+
 	[[ -s $(brew --prefix)/etc/autojump.sh ]] && . $(brew --prefix)/etc/autojump.sh
 fi
 
