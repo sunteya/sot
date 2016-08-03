18d98d3123fe40264ee833682a4a29898c002740
sunteya <sunteya@gmail.com>
2016-08-03 12:05:53 +0800

    fix broken on bash v3

--- bash/darwin.sh
+++ bash/darwin.sh
@@ -4,7 +4,7 @@
 if [ $(type -P brew) ] ; then
 	[ -f `brew --prefix`/etc/bash_completion ] && source `brew --prefix`/etc/bash_completion
 
-	if [ -f $(brew --prefix)/share/bash-completion/bash_completion  ]; then
+	if [ "$BASH_VERSINFO" == "4" ] && [ -f $(brew --prefix)/share/bash-completion/bash_completion  ]; then
 		source $(brew --prefix)/share/bash-completion/bash_completion
 	fi
 
